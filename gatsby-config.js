82f5e42cfe788d8968cf216853309a893d48f596
j-yama <j-yama@example.com>
2018-10-10 23:08:24 +0900

    簡単なAboutページを作成した

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
-module.exports{
-    plugins: ['gatsby-plugin-flow']
+module.exports = {
+  plugins: ['gatsby-plugin-flow']
 };
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>About</h1>
+    <p>ネコミミのネコミミによるネコミミのためのページ！</p>
+  </div>
+);
