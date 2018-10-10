2eb79ef9bf938b894bcf83a28300d4396bcebae6
j-yama <j-yama@example.com>
2018-10-10 23:11:41 +0900

    ホームページにAboutページへのリンクを追加した

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
 // @flow
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <div>
+    <h1>Hello world!</h1>
+    Go to <Link to="/about">About</Link> page
+  </div>
+);
